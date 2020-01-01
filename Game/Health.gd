f7c191cee395d96a61eb58ebc206f8e81c3ad46e
Geoffrey Swisher <geoffreydswisher@gmail.com>
2020-01-01 13:51:32 -0500

    added tileset

---
+++ Game/Health.gd
@@ -0,0 +1,5 @@
+var player_health = 100
+var enemy_health = 30
+
+func getPlayerHealth():
+	return player_health
--- Game/Player1.gd
+++ Game/Player1.gd
@@ -6,20 +6,34 @@ var gravity = 1200
 
 var velocity = Vector2()
 
+var Health = load("res://Game/Health.gd")
+var health = Health.new()
+
 func get_input():
+
 	velocity.x = 0
+
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += speed
+
 	if Input.is_action_pressed("ui_left"):
 		velocity.x += -speed
+
 	if is_on_floor() and Input.is_action_pressed("ui_up"):
 		velocity.y += jumpspeed
-	
+
+	if Input.is_action_just_pressed("game_x"):
+		print('kick')
+
+	if Input.is_action_just_pressed("game_z"):
+		print('punch')
 
 func _physics_process(delta):
-	print(delta)
+	
 	velocity.y += gravity * delta
+	
 	get_input()
+	
 	velocity = move_and_slide(velocity, Vector2(0,-1))
 
 
